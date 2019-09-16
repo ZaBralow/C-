4dc3cdce6a386fe9d7c505235f9d92809d6681dd
ZaBralow <pavlik.elsukov@mail.ru>
2019-09-16 10:47:33 +0300

    true

---
+++ complete.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() {
+	
+	
+	int Seine = 32;
+	float Garrone = 32;
+	wchar_t Loire = 16;
+	char Meuse[] = {"Meuse"};
+	bool Rhin = true;
+	double Lyon = 64;
+	
+	
+	cout << "int - " << Seine << endl;
+	cout << "float - " << Garrone << endl;
+	cout << "wchart_t - " << Loire << endl;
+	cout << "double - " << Lyon << endl;
+	cout << "char - " << Meuse << endl;
+	cout << "bool - " << Rhin << endl;
+	
+	
+	
+
+	
+	return 0;
+}
