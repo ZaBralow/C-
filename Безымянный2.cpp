7829cde9af2f976eabbc537275733d401a0b3e39
ZaBralow <pavlik.elsukov@mail.ru>
2019-09-16 10:14:15 +0300

    c++

---
+++ Безымянный2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>;
+using namespace std;
+int main() {
+	int Seine = 1, Garrone = 8, Loire = 16, Lyon = 64, Meuse = 32, Rhin = 32;
+	
+	cout << "bool - " << Seine << endl;
+	cout << "char - " << Garrone << endl;
+	cout << "wchart_t - " << Loire << endl;
+	cout << "double - " << Lyon << endl;
+	cout << "float - " << Meuse << endl;
+	cout << "int - " << Rhin << endl;
+	
+	return 0;
+}
